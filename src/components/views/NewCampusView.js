1e09eae7a6178b3613b3bffcfa08037e943813a1
kevinville <kevinlhardyal@gmail.com>
2022-12-09 16:43:16 -0500

    added useState to newcampusview

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -4,6 +4,7 @@ NewCampusView.js - taken from newstudentview.js
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
 
 // Create styling for the input form
 const useStyles = makeStyles( () => ({
