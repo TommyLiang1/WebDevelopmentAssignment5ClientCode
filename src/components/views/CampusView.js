24dac8385d6ae250cbb80b722a04a470a8f2470a
kevinville <kevinlhardyal@gmail.com>
2022-12-09 16:24:18 -0500

    Added student count for campus

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -4,20 +4,36 @@ CampusView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
+// implemented useState to be used as real-time error handling
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
   const {campus, deleteCampus} = props;
-  
-  // if there is no campus
+  // two empty msgs for real time error handling
+  const {campusdeletemsg, setcampusdeletemsg} = useState("");
+  // temp variable to check student enrollment
+  let temp;
+  // to set user image
+  let image;
+
+  // if there is no campus or incorrect id
   if(campus === null){
     return(
       <div>You entered an incorrect ID or this campus does not exist.</div>
     )
   }
 
+  //checking student enrollment
+  if(campus.students.length === 0){
+    temp = "No students are at this campus.";
+  }
+  else{
+    temp = "Students at this campus:";
+  }
 
+  
   // Render a single Campus view with list of its students
   return (
     <div>
